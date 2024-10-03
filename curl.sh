eb712f70ec9fae21ac07d410fb4724cd86c5e0f1
SachinPereira <56588858+SachinPereira@users.noreply.github.com>
2024-10-03 17:12:48 +0530

    Create curl.sh

---
+++ curl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# URL of the YouTube video
+VIDEO_URL="https://www.youtube.com/shorts/B6ebIDCVXW8" # Replace with your YouTube video URL
+
+# Use curl to open the YouTube video URL
+curl -L $VIDEO_URL
