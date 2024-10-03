0c54a785712947594dd7f0889b845f951d6ca7c3
SachinPereira <56588858+SachinPereira@users.noreply.github.com>
2024-10-03 17:16:18 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -3,5 +3,10 @@
 # URL of the YouTube video
 VIDEO_URL="https://www.youtube.com/shorts/B6ebIDCVXW8" # Replace with your YouTube video URL
 
-# Use curl to open the YouTube video URL
-curl -L $VIDEO_URL
+# Run the YouTube video 10 times
+for i in {1..10}
+do
+  echo "Running YouTube video - Attempt $i"
+  curl -L $VIDEO_URL
+  sleep 5 # Delay of 5 seconds between each run
+done
